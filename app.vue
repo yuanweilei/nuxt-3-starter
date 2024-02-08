9eb824c112134e14e35e214974a9e6acc5cc95d0
Yuanwei Lei <lywwind@gmail.com>
2024-02-08 22:46:01 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,19 +2,7 @@
   <div>
     <div class="home-page">
       <h1>Hi! This is a simple Nuxt 3 app.</h1>
-      <h2>Click on the buttons below to check out a server route or an API route :) </h2>
-      <NuxtLink to="/hello" target="_blank">
-        <button> 
-            What Time Is It?
-        </button>
-      </NuxtLink>
-      <NuxtLink to="/api/hello" target="_blank">
-        <button>
-            Link to API Route
-        </button>
-      </NuxtLink>
-      <h2> Deploy Nuxt 3 app with universal rendering on Azure Static Web Apps using <NuxtLink to="https://docs.microsoft.com/azure/static-web-apps/deploy-nuxtjs" class="styling">the Microsoft documentation</NuxtLink></h2>
-    </div>
+     </div>
     <NuxtWelcome />
   </div>
 </template>
